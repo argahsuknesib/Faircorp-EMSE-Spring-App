47a71d0e3a0e12b41cb13d7943236c8cefe43fdc
Kushagra Singh Bisen <k.bisen@yahoo.com>
2021-11-16 11:31:40 +0100

    working,there are some specific query errors

--- bin/main/data.sql
+++ bin/main/data.sql
@@ -8,30 +8,24 @@ INSERT INTO ROOM(id, name, floor, current_temperature, target_temperature, build
 INSERT INTO ROOM(id, name, floor, current_temperature, target_temperature, building_id) VALUES(-6, 'Room3', 2, 21.8, 20.0 ,  -10);
 INSERT INTO ROOM(id, name, floor, current_temperature, target_temperature, building_id) VALUES(-5, 'Room3', 3, 20.6, 20.0 ,  -9);
 
--- INSERT INTO HEATER(id, name, power, room_id, heater_status) VALUES(2, 'Heater1', 765, -4, 'ON');
 
+INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-1, 'ON', 'Heater1', 2000, -10);
+INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-9, 'ON', 'Heater2', null, -10);
+INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-14, 'OFF', 'Heater1', 2000, -10);
+INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-3, 'ON', 'Heater2', null, -10);
+INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-2, 'ON', 'Heater4', null, -10);
+INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-5, 'ON', 'Heater2', null, -9);
 
-
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-1, 'ON', 'Heater1', 2000, -10);
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-9, 'ON', 'Heater2', null, -10);
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-14, 'OFF', 'Heater1', 2000, -10);
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-3, 'ON', 'Heater2', null, -10);
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-3, 'OFF', 'Heater3', 2000, -10);
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-2, 'ON', 'Heater4', null, -10);
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-1, 'OFF', 'Heater1', 2000, -9);
--- INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-5, 'ON', 'Heater2', null, -9);
-
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-10, 'CLOSED', 'Window 1', -10);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-9, 'CLOSED', 'Window 2', -10);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-8, 'OPEN', 'Window 1', -9);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-7, 'CLOSED', 'Window 2', -9);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-5, 'CLOSED', 'Window 2', -8);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-4, 'OPEN', 'Window 1', -7);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-3, 'CLOSED', 'Window 2', -7);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-2, 'OPEN', 'Window 1', -6);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-1, 'CLOSED', 'Window 2', -6);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-11, 'OPEN', 'Window 1', -5);
--- INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-12, 'CLOSED', 'Window 2', -5);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-10, 'CLOSED', 'Window 1', -10);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-9, 'CLOSED', 'Window 2', -10);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-8, 'OPEN', 'Window 1', -9);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-7, 'CLOSED', 'Window 2', -9);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-4, 'OPEN', 'Window 1', -7);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-3, 'CLOSED', 'Window 2', -7);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-2, 'OPEN', 'Window 1', -6);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-1, 'CLOSED', 'Window 2', -6);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-11, 'OPEN', 'Window 1', -5);
+INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-12, 'CLOSED', 'Window 2', -5);
 
 
 
